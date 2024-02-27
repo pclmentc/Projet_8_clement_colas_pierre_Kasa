177cca47da6abbfe247bf7d7f42e5ff0dd242927
pclmentc <pclmentc@gmail.com>
2024-02-27 14:22:41 +0100

    ajout erreur 404

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -5,6 +5,7 @@ import Accueil from './pages/Accueil/Accueil'
 
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import Error404 from './pages/404'
 
 
 function Router() {
@@ -12,17 +13,12 @@ function Router() {
 
   return (
     <React.StrictMode>
-			<BrowserRouter basename="/Kasa">
+			<BrowserRouter basename="/">
 				<Navbar />
 				<Routes>
 					
 					<Route exact path="/" element={<Accueil />} />
-					
-					{/* <Route exact path="/annonce/:annonceId"	element={<Annonce />}	/>
-					
-					<Route exact path="/a-propos" element={<APropos />} />
-					
-					<Route path="*" element={<Error404 />} /> */}
+					<Route path="*" element={<Error404 />} />
           
 				</Routes>
 				<Footer />
