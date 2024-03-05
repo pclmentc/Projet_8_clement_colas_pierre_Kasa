7b19239337072b067fd006d5e30d87515b601272
pclmentc <pclmentc@gmail.com>
2024-03-05 10:20:53 +0100

    collapse + rotation icon

---
+++ front/src/components/Collapse/Collapse.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useRef } from 'react'
+import chevron from '../../assets/chevron.svg'
+import './Collapse.scss'
+
+function Collapse({ title, content }) {
+    // État pour gérer la classe 'active'
+    const [setActive, setActiveState] = useState('')
+    
+    // État pour gérer la hauteur du contenu
+    const [setHeight, setHeightState] = useState('0px')
+    
+    // État pour gérer la rotation de l'icône chevron
+    const [setRotate, setRotateState] = useState('collapse-icon')
+
+    // Référence pour obtenir la hauteur du contenu
+    const contentCollapse = useRef(null)
+
+    // Fonction pour basculer l'état du collapse
+    const toggleCollapse = () => {
+        setActiveState(setActive === '' ? 'active' : '')
+        setHeightState(
+            setActive === 'active'
+                ? '0px'
+                : `${contentCollapse.current.scrollHeight}px`
+        )
+        setRotateState(
+            setActive === 'active' ? 'collapse-icon' : 'collapse-icon rotate'
+        )
+    }
+
+    // Tableau pour stocker le contenu, gérant à la fois le contenu unique et le tableau de contenu
+    const contentArray = []
+    if (!Array.isArray(content)) {
+        contentArray.push(content)
+    } else {
+        for (let i = 0; i < 9; i++) {
+            contentArray.push(content[i])
+        }
+    }
+
+    return (
+        <div className="collapse-section">
+            {/* Bouton pour basculer le collapse */}
+            <button
+                className={`collapse ${setActive}`}
+                onClick={toggleCollapse}
+            >
+                <span className="collapse-title">{title}</span>
+                <img src={chevron} className={`${setRotate}`} alt="" />
+            </button>
+
+            {/* Zone de contenu qui se développe/rétracte */}
+            <div
+                ref={contentCollapse}
+                style={{ maxHeight: `${setHeight}` }}
+                className="collapse-content"
+            >
+                {/* Affichage du contenu en fonction de contentArray */}
+                <div className="collapse-text">
+                    {contentArray.map((content, index) => (
+                        <div key={`${content}-${index}`}>{content}</div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Collapse
--- front/src/pages/A-propos/Apropos.jsx
+++ front/src/pages/A-propos/Apropos.jsx
@@ -1,11 +1,45 @@
 import './Apropos.scss'
+import Collapse from '../../components/Collapse/Collapse';
 
 function Apropos() {
     document.title = 'A-propos - Kasa'
+    const aproposData = [
+		{
+			title: 'Fiabilité',
+			content:
+				'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
+		},
+		{
+			title: 'Respect',
+			content:
+				'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+		},
+		{
+			title: 'Service',
+			content:
+				"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+		},
+		{
+			title: 'Sécurité',
+			content:
+				"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+		},
+	]
     return (
-
-        <div className='backImg1'>            
+<section>
+        <div className='backImg1'>
         </div>
+<div className='collapseSection'>
+        {aproposData.map((apropos, index) => (
+				<Collapse
+					title={apropos.title}
+					content={apropos.content}
+					key={`${apropos.title}-${index}`}
+				/>
+			))} 
+</div>
+</section>
+        
     )
 }
 
